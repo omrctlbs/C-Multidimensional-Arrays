0973dcad28e903e7934b6f5cf020970c748a435a
Omer Catalbas <78023319+omrctlbs@users.noreply.github.com>
2024-07-29 00:32:07 +0300

    C# Multidimensional-Arrays

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] names = { "Ali", "Ahmet", "Can" };
+int[,] notes = new int[3, 3];
+notes[0, 0] = 50;
+notes[0, 1] = 60;
+notes[0, 2] = 70;
+notes[1, 0] = 60;
+notes[1, 1] = 80;
+notes[1, 2] = 90;
+notes[2, 0] = 50;
+notes[2, 1] = 70;
+notes[2, 2] = 30;
+
+var alinotes = (notes[0, 0] + notes[0, 1] + notes[0, 2]) / 3;
+Console.WriteLine("Ali Notes Average: " + alinotes);
+
+var ahmetnotes = (notes[1, 0] + notes[1, 1] + notes[1, 2]) / 3;
+Console.WriteLine("Ahmet Notes Average: " + ahmetnotes);
+
+var cannotes = (notes[2, 0] + notes[2, 1] + notes[2, 2]) / 3;
+Console.WriteLine("Can Notes Average: " + cannotes);
